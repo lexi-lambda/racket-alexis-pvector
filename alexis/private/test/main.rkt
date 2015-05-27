c5903ee9f9e3e5edf0d8dcba5f6157d7b85784f4
Alexis King <lexi.lambda@gmail.com>
2015-05-27 16:56:55 -0700

    Initial commit

---
+++ alexis/private/test/main.rkt
@@ -0,0 +1,13 @@
+#lang racket/base
+
+(require
+  rackunit
+  alexis/collection
+  alexis/pvector)
+
+(test-case
+ "Basic vector operations"
+ (check-equal? (sequence->list (pvector 1 2 3 4)) '(1 2 3 4))
+ (check-equal? (sequence->list (conj* (pvector) 1 2 3 4)) '(1 2 3 4))
+ (check-equal? (sequence->list (rest (pvector 1 2 3 4))) '(2 3 4))
+ (check-equal? (sequence->list (reverse (pvector 1 2 3 4))) '(4 3 2 1)))
---
+++ info.rkt
@@ -0,0 +1,12 @@
+#lang info
+
+(define collection 'multi)
+
+(define name "alexis-pvector")
+(define version "0.1.0")
+
+(define deps
+  '("base"
+    "alexis-collections"))
+(define build-deps
+  '("cover"))
